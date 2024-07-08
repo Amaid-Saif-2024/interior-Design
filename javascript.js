7bde7d53d1f113a26505b2216f78bccd4350566d
Annas niaz <168002195+20030878@users.noreply.github.com>
2024-07-08 10:42:38 +0100

    functon

--- javascript.js
+++ javascript.js
@@ -1,1 +1,39 @@
-document get IdleDeadline
+//document.getElementById('get-started-btn').addEventLitener('click, function() {
+//document.getElementById('description-section').style.display = 'none';
+//document.getElementById('form-section').style.display = 'block';
+// });
+
+document.getElementById('back-btn').addEventListener('click',function() {
+    document.getElementById('form-section').style.display = 'none';
+    document.getElementById('decription-section').style.display = 'block';
+});
+
+document.getElementById('back-to-form-btn').addEvntListener('click',function() {
+    document.getElementById('suggestions-section').style.display = 'none';
+    document.getElementById('form-section').style.display = 'block';
+});
+
+function showFeature(feature) {
+    document.getElementById('description-section').style.display = 'none';
+    document.getElementById('form-section').style.display = 'block';
+
+    let featureTitle = '';
+    switch (feature) {
+        case 'paint':
+            featureTitle = 'Change Paint';
+            break;
+        case 'furniture':
+            featureTitle = 'Add Furniture';
+            break;
+        case 'windows':
+            featureTitle = 'Add Windows';
+            break;
+        case 'tiles':
+            featureTitle = 'Add Tiles';
+            break;
+        default:
+            featureTitle = 'Virtual Interior Design'
+    }
+    document.getElementById('feature-title').innerText = featureTitle;
+    document.getElementById('selected-feature').value = feature;
+}
