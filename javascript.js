8e0c452f2dba64726baa1437df3d84e4489e1163
Annas niaz <168002195+20030878@users.noreply.github.com>
2024-07-18 21:12:58 +0100

    Remove commit lines

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-//document.getElementById('get-started-btn').addEventLitener('click, function() {
-//document.getElementById('description-section').style.display = 'none';
-//document.getElementById('form-section').style.display = 'block';
-// });
-
 document.getElementById('back-btn').addEventListener('click',function() {
     document.getElementById('form-section').style.display = 'none';
     document.getElementById('decription-section').style.display = 'block';
